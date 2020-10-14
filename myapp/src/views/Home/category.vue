d6b1fa9f2db6265b222d774b3acc0d55d753738e
SimonXu4fun <xwh90@163.com>
2020-10-15 00:53:39 +0800

    add myapp

---
+++ myapp/src/views/Home/category.vue
@@ -0,0 +1,72 @@
+<template>
+	<div>
+		<ul class="cate-box">
+			<li class="cate-list" v-for="obj in cateList" :key="obj.id">
+				<img class="cate-img" :src="obj.img"/>
+				<p class="cate-title">{{obj.name}}</p>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+	export default{
+		data(){
+			return{
+				cateList:[]
+			}
+		},
+		created: function() {
+			axios.get('./get_type.json').then(res =>{
+				// console.log(res);
+				//
+				if(res.data.errcode == 200){
+					// console.log(res.data.data);
+					this.cateList = res.data.data;
+					/* for(let ob of this.cateList){
+						console.log(ob.img);
+					} */
+					
+				}else{
+					console.log(res.data.errmsg)
+				}
+			}).catch(res=>{
+				console.log(res);
+			})
+			/* var xmlhttp;
+			xmlhttp=new XMLHttpRequest();
+			xmlhttp.onreadystatechange=function()
+			{
+				if (xmlhttp.readyState==4 && xmlhttp.status==200){
+					console.log(xmlhttp.responseText);
+					var res = JSON.parse(xmlhttp.responseText);
+					console.log(res);
+					this.cateList = res.data;
+				}
+				
+			}
+			xmlhttp.open("GET","./get_type.json",true);
+			xmlhttp.send(); */
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.cate-box{
+		display: flex;
+		flex-wrap: wrap;
+		.cate-list{
+			width: 20%;
+			text-align: center;
+			padding-top: 0.3rem;
+			.cate-img{
+				width: 1rem;
+				height: 1rem;
+			}
+			.cate-title{
+				font-size: 12px;
+			}
+		}
+	}
+</style>
---
+++ myapp/src/views/Home/index.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<!-- header -->
+		<van-sticky :offset-top="0">
+			<div class="header">
+				<input type="text" class="search" placeholder="请输入商家或商品名称"/>
+			</div>
+		</van-sticky>
+		<category></category>
+	</div>
+</template>
+
+<script>
+	import category from './category.vue'
+	export default{
+		components:{
+			category
+			}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.header{
+		background: #1c1b20;
+		padding: 0.2rem;
+		.search{
+			width: 100%;
+			border: none;
+			height: 0.6rem;
+			line-clamp: 0.6rem;
+			border-radius: 0.3rem;
+			padding: 0 0.2rem;
+			box-sizing: border-box;
+			font-size: 0.28rem;
+		}
+	}
+</style>
